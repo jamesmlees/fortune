6e66d0dffe7ed213f044a5d777a5f06674ecd11d
Craig Edmunds <craig.edmunds@gmail.com>
2015-01-16 13:22:00 +0000

    Added mechanism to create DB if it doesn't exist

--- test/databases/couchdb.spec.js
+++ test/databases/couchdb.spec.js
@@ -1,6 +1,6 @@
 var should = require('should');
 
-var Adapter = require('../../lib/adapters/couchdb');
+var adapterInit = require('../../lib/adapters/couchdb');
 
 var RSVP = require('rsvp');
 var Promise = RSVP.Promise;
@@ -11,29 +11,30 @@ RSVP.on('error', function (err) {
     throw err;
 });
 
-//module.exports = function (options) {
+describe('CouchDB adapter', function () {
+  var ids;
 
-    describe('CouchDB adapter', function () {
-        var ids;
-
-        describe('Creation', function () {
-            it('should be able to create document with provided id', function (done) {
-              
-                var adapter = Adapter({ host: process.env.WERCKER_COUCHDB_HOST || 'localhost', port: process.env.WERCKER_COUCHDB_PORT ||5984})
-                var doc = {
-                    id: '123456789012345678901234'
-                };
-                
-                adapter.create(doc.id, doc).then(function () {
-                    adapter.find('', doc.id).then(function (doc) {
-                        should.exist(doc);
-                        done();
-                    });
-                });
-                
-            });
+  describe('Creation', function () {
+    it('should be able to create document with provided id @now', function (done) {
+      
+      adapterInit({
+        host: process.env.WERCKER_COUCHDB_HOST || 'localhost',
+        port: process.env.WERCKER_COUCHDB_PORT || 5984
+      })
+      .then(function(adapter) {
+        console.log("adapter initialised", adapter);
+        var doc = {
+            id: '123456789012345678901234'
+        };
+        
+        adapter.create(doc.id, doc).then(function () {
+          adapter.find('', doc.id).then(function (doc) {
+            should.exist(doc);
+            done();
+          });
         });
-
+      })
     });
+  });
 
-//};
+});
